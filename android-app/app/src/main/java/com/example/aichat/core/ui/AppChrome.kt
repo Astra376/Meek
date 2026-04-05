9e2e215c08a36413faa555cff1524e05c9c6cbcc
= <=>
2026-04-06 00:09:22 +0930

    edit

--- android-app/app/src/main/java/com/example/aichat/core/ui/AppChrome.kt
+++ android-app/app/src/main/java/com/example/aichat/core/ui/AppChrome.kt
@@ -45,10 +45,10 @@ object AppChrome {
     val compactControlGap = 10.dp
     val compactHeaderVerticalPadding = 8.dp
     val headerActionIconSize = 24.dp
-    val bottomBarHeight = 29.dp
-    val bottomBarTapHeight = 29.dp
+    val bottomBarHeight = 41.dp
+    val bottomBarTapHeight = 41.dp
     val bottomBarHorizontalPadding = 16.dp
-    val bottomBarVerticalPadding = 0.dp
+    val bottomBarVerticalPadding = 4.dp
     val bottomBarItemHorizontalPadding = 4.dp
     val bottomBarIconSize = 28.dp
     val listRowGap = 20.dp

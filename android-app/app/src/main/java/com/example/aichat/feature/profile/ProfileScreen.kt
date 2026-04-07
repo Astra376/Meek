89bdfeb088945eb95c9b230fe1899a5d6abb51a1
Astra376 <connor.andrews81@outlook.com>
2026-04-07 16:36:17 +0930

    new

--- android-app/app/src/main/java/com/example/aichat/feature/profile/ProfileScreen.kt
+++ android-app/app/src/main/java/com/example/aichat/feature/profile/ProfileScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
@@ -145,6 +146,7 @@ class ProfileViewModel @Inject constructor(
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ProfileRoute(
     paddingValues: PaddingValues,
